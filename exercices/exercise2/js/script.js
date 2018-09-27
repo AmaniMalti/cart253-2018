0bd8617dfa6dd164165d42295a451fdd49219903
Amani Malti <a_alti@cda082ev5815x.local>
2018-09-27 15:27:57 -0400

    modified the comments to the script
    added the names of my images

--- exercices/exercise2/js/script.js
+++ exercices/exercise2/js/script.js
@@ -51,16 +51,16 @@ function preload() {
 
 // setup()
 //
-// Make the canvas, position the avatar and anemy
+// Make the canvas, position the kot image and the kotkot image
 function setup() {
   // Create our playing area
   createCanvas(500,500);
 
-  // Put the avatar in the centre
+  // Put the kot image in the centre
   kotImageX = width/2;
   kotImageY = height/2;
 
-  // Put the enemy to the left at a random y coordinate within the canvas
+  // Put the kotkot image to the left at a random y coordinate within the canvas
   kotkotImageX = 0;
   kotkotImageY = random(0,height);
 
@@ -74,10 +74,9 @@ function setup() {
 // game over situations.
 function draw() {
   // Add light blue background
-  //background(255,220,220);//
   background('#ccffff');
 
-  // Added text
+  // Add text
   text('DODGES '+dodges,400,50);
   textAlign(CENTER);
   textSize(20);
@@ -86,11 +85,11 @@ function draw() {
 
 
 
-  // Default the avatar's velocity to 0 in case no key is pressed this frame
+  // Default the kot image velocity to 0 in case no key is pressed this frame
   kotImageVX = 0;
   kotImageVY = 0;
 
-  // Check which keys are down and set the avatar's velocity based on its
+  // Check which keys are down and set the kot image velocity based on its
   // speed appropriately
 
   // Left and right
@@ -110,35 +109,35 @@ function draw() {
     kotImageVY = kotImageSpeed;
   }
 
-  // Move the avatar according to its calculated velocity
+  // Move the kot image according to its calculated velocity
   kotImageX = kotImageX + kotImageVX;
   kotImageY = kotImageY + kotImageVY;
 
-  // The enemy always moves at enemySpeed (which increases)
+  // The kotkot image always moves at enemySpeed (which increases)
   kotkotImageVX = kotkotImageSpeed;
-  // Update the enemy's position based on its velocity
+  // Update the kotkot image position based on its velocity
   kotkotImageX = kotkotImageX + kotkotImageVX;
 
-  // Check if the enemy and avatar overlap - if they do the player loses
-  // We do this by checking if the distance between the centre of the enemy
-  // and the centre of the avatar is less that their combined radii
+  // Check if the kotkot image and avatar overlap - if they do the player loses
+  // We do this by checking if the distance between the centre of the kotkot image
+  // and the centre of the kot image is less that their combined radii
   if (dist(kotkotImageX,kotkotImageY,kotImageX,kotImageY) < kotkotImageSize/2 + kotkotImageSize/2) {
     // Tell the player they lost
     console.log("YOU LOSE!");
-    // Reset the enemy's position
+    // Reset the kot image's position
     kotkotImageX = 0;
     kotkotImageY = random(0,height);
-    // Reset the enemy's size and speed
+    // Reset the kotkot image size and speed
     kotkotImageSize = 50;
     kotkotImageSpeed = 5;
-    // Reset the avatar's position
+    // Reset the kot image position
     kotImageX = width/2;
     kotImageY = height/2;
     // Reset the dodge counter
     dodges = 0;
   }
 
-  // Check if the avatar has gone off the screen (cheating!)
+  // Check if the kot image has gone off the screen (cheating!)
   if (kotImageX < 0 || kotImageX > width || kotImageY < 0 || kotImageY > height) {
     // If they went off the screen they lose in the same way as above.
     console.log("YOU LOSE!");
@@ -151,16 +150,16 @@ function draw() {
     dodges = 0;
   }
 
-  // Check if the enemy has moved all the way across the screen
+  // Check if the kotkot image has moved all the way across the screen
   if (kotkotImageX > width) {
     // This means the player dodged so update its dodge statistic
     dodges = dodges + 1;
     // Tell them how many dodges they have made
     console.log(dodges + " DODGES!");
-    // Reset the enemy's position to the left at a random height
+    // Reset the kotkot image position to the left at a random height
     kotkotImageX = 0;
     kotkotImageY = random(0,height);
-    // Increase the enemy's speed and size to make the game harder
+    // Increase the kotkot image speed and size to make the game harder
     kotkotImageSpeed = kotkotImageSpeed + kotkotImageSpeedIncrease;
     kotkotImageSize = kotkotImageSize + kotkotImageSizeIncrease;
   }
@@ -176,10 +175,10 @@ function draw() {
   // Display the current number of successful in the console
   console.log(dodges);
 
-  // Display the chick image
+  // Display the kot image
   image(kot,kotImageX,kotImageY);
 
-  // Display the enemy chick image
+  // Display the kotkot image
   image(kotkot,kotkotImageX,kotkotImageY);
 
 }
