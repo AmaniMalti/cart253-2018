5f56896514239b65262bea5074261127842cc313
Amani <amanimalti.95@gmail.com>
2018-10-31 18:48:10 -0400

    EX 6: Fixed syntax error
    Ball.js line 59: added a comma between this.x , this.y

--- exercises/broken-game-basic-oop-pong/js/Ball.js
+++ exercises/broken-game-basic-oop-pong/js/Ball.js
@@ -54,8 +54,9 @@ Ball.prototype.isOffScreen = function () {
 // display()
 //
 // Draw the ball as a rectangle on the screen
+//////////////// FIXED
 Ball.prototype.display = function () {
-  rect(this.x this.y);
+  rect(this.x, this.y);
 }
 
 // handleCollision(paddle)
