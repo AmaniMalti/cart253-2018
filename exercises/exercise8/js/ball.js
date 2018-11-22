67c4386601c84033299ce439bae8b0123fc294ad
Amani <amanimalti.95@gmail.com>
2018-11-21 21:20:44 -0500

    EX8 Adding the bouncing ball
    Adding the ball object that bounces inside the canvas

---
+++ exercises/exercise8/js/ball.js
@@ -0,0 +1,46 @@
+// Ball
+//
+// A class to define how a ball behaves when reaching the edges of the canvas: it will just bounce
+
+// Ball constructor
+//
+// Sets the properties with the provided arguments
+function Ball(x, y, vx, vy, size, speed) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.size = size;
+  this.speed = speed;
+  this.fill = 100;
+}
+
+// update()
+//
+// Moves according to velocity, constrains y to be on screen,
+// checks for bouncing on upper or lower edgs, checks for going
+// off left or right side.
+Ball.prototype.update = function() {
+  // Update position with velocity
+  this.x += this.vx;
+  this.y += this.vy;
+
+  // Constrain y position to be on screen
+  this.y = constrain(this.y, 0, height - this.size);
+
+  // Check for touching upper or lower edge and reverse velocity if so
+  if (this.y === 0 || this.y + this.size === height) {
+    this.vy = -this.vy;
+  }
+    // Check for touching left or right edge and reverse velocity if so
+  if (this.x === 0 || this.x + this.size === width) {
+    this.vx = -this.vx;
+  }
+}
+
+// display()
+//
+// Draw the ball as a rectangle on the screen
+Ball.prototype.display = function() {
+  ellipse(this.x, this.y, this.size, this.size);
+}
--- exercises/exercise8/js/script.js
+++ exercises/exercise8/js/script.js
@@ -3,6 +3,9 @@ var eyeSize = 100;
 var eyeBallSize = 50;
 var eyeShapeSize = 110;
 var eyeBallColour = 255;
+// Adding variable for new the new ball object
+var ball;
+
 
 function setup() {
   // Make the canvas fullscreen
@@ -11,16 +14,22 @@ function setup() {
   leftEye = new Eyes(width/2 - 80);
   // Create right eye
   rightEye = new Eyes(width/2 + 80);
+  // Create a ball
+  ball = new Ball(width / 2, height / 2, 20, 25, 40, 150);
 
 }
 function draw() {
   background('#fae');
+  // Updating the ball position
+  ball.update();
   // Drawing the eyes in their position
   strokeWeight(1);
   leftEye.drawEyes();
   rightEye.drawEyes();
   // showing the text
   showText();
+  // Displaying the ball
+  ball.display();
 }
 // Display the text
 function showText(){
