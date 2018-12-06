9f2bd512e7d810ea5c588b0e9707ea015d1ce8f5
Amani <amanimalti.95@gmail.com>
2018-12-05 23:16:09 -0500

    P3: replaced fly.js by ball.js
    replaced fly.js by ball.js because my images of flies did not show up when I was trying to put them in a loop. There is maybe a bug with the librarie when we try to put an image in a loop because I kept getting a indefined error message

--- exercises/Project3/js/ball.js
+++ exercises/Project3/js/ball.js
@@ -1,33 +1,35 @@
-// fly
-// A class to define how the flys would behave when reaching the edges of the canvas: they will just bounce
+// ball
+// A class to define how the balls would behave when reaching the edges of the canvas: they will just bounce
 
-// fly constructor
+// ball constructor
 // Sets the properties with the provided arguments
-function fly(x, y, vx, vy, size, speed,tx,ty,imgfly) {
+function ball(x,y,vx,vy,size,speed,tx,ty,r,g,b,dist) {
   this.x = x;
   this.y = y;
   this.vx = vx;
   this.vy = vy;
-  this.size = 60;
+  this.size = size;
   this.speed = speed;
   this.tx = tx;
   this.ty = ty;
-  this.fill = 100;
-  this.imgfly = imgfly;
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  this.dist = dist;
 }
 
 // update()
 // Moves according to velocity, constrains y to be on screen,
 // checks for bouncing on upper or lower edgs, checks for going
 // off left or right side.
-fly.prototype.update = function() {
+ball.prototype.update = function() {
   // Update position with velocity
   this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
-  this.y = constrain(this.y, 0, height - this.size);
-  this.x = constrain(this.x, 0, width - this.size);
+  this.y = constrain(this.y, this.size/2, height - this.size/2);
+  this.x = constrain(this.x, this.size/2, width - this.size/2);
 
   // Check for touching upper or lower edge and reverse velocity if so
     if (this.x < 0) {
@@ -46,24 +48,31 @@ fly.prototype.update = function() {
 
 // display()
 //
-// Draw the fly
-fly.prototype.display = function() {
-  image(this.imgfly,this.x, this.y, this.size, this.size);
+// Draw the ball
+ball.prototype.display = function() {
+  imageMode(CENTER);
+  push();
+  noStroke();  
+  fill(this.r,this.g,this.b);
+  //image(this.imgball,this.x, this.y, this.size, this.size);
+  //image(this.imgball,0, 0, 10, 10);
+  ellipse(this.x, this.y, this.size, this.size);
+   pop();
 }
 
 // Angle calculation
-fly.prototype.followMe = function() {
+ball.prototype.followMe = function() {
   a = atan2(this.y - height/2, this.x - width/2);
   return a;
 }
 
 // Proportional distance
-fly.prototype.howFarIs = function() {
-  length = dist(this.x, this.y, width/2, height/2)/15;
+ball.prototype.howFarIs = function() {
+  length = dist(this.x, this.y, width/2, 6.2 *height/8)/15;
   return length;
 }
 
-fly.prototype.calculateVelocity = function() {
+ball.prototype.calculateVelocity = function() {
   this.vx = map(noise(this.tx),0,1,-maxSpeed,maxSpeed);
   this.vy = map(noise(this.ty),0,1,-maxSpeed,maxSpeed);
 }
--- exercises/project3/js/script.js
+++ exercises/project3/js/script.js
@@ -10,31 +10,29 @@ var eyeBallSize = 50;
 var eyeShapeSize = 110;
 var eyeBallColour = 255;
 // Variable for fly object
-var fly;
-var fly1;
-var fly2;
-var fly3;
-var fly4;
-var fly5;
+var balls = [];
+var num_balls = 25;
+var img = [];
+var distances = [];
+var j=0;
+
 // Variable that defines max speed
 var maxSpeed = 15;
 // Variable that calculates the angle (in radians) from the fly position to the coordinate origin as measured from the positive x-axis
 var a = 0;
 // A fraction of the distance between the fly and the the center of the canvas
-var length = 0;
+var length = 30;
+
 
 function preload() {
-  // Adding image of fly to replace the fly
-  img1 = loadImage('assets/images/blackFly.png');
-  img2 = loadImage('assets/images/greyFly.png');
-  img3 = loadImage('assets/images/greenFly.png');
-  img4 = loadImage('assets/images/redFly.png');
-  img5 = loadImage('assets/images/purpleFly.png');
-
+  // Background image
+img = loadImage('assets/images/bg1.jpg');
 }
+
 
 function setup() {
   // Create a canvas the size of the window
+  //createCanvas(windowWidth,windowHeight);
   canvas = createCanvas(windowWidth,windowHeight);
   // Style it so that it sits behind the main HTML in a fixed position that ignores scrolling
   canvas.style("display:block");
@@ -46,89 +44,79 @@ function setup() {
   leftEye = new Eyes(width/2 - 80);
   // Create right eye
   rightEye = new Eyes(width/2 + 80);
-  // Create the flys
-  fly1 = new fly(width / 2, height / 2, 20, 25, 40, 150,random(0,1000), random(0,1000),img1);
-  fly2 = new fly(width / 2, height / 2, 20, 25, 40, 150,random(0,1000), random(0,1000),img2);
-  fly3 = new fly(width / 2, height / 2, 20, 25, 40, 150,random(0,1000), random(0,1000),img3);
-  fly4 = new fly(width / 2, height / 2, 20, 25, 40, 150,random(0,1000), random(0,1000),img4);
-  fly5 = new fly(width / 2, height / 2, 20, 25, 40, 150,random(0,1000), random(0,1000),img5);
-
+    // Create the colored balls
+  for (var i = 0; i < num_balls; i++) {
+    balls.push(new ball(random(0,width),random(0,height),20,25,40,150,random(0,1000),random(0,1000),getRandomInt(255),getRandomInt(255),getRandomInt(255),0));
+  }
+  	console.log(balls[1].r,balls[1].g,balls[1].b);
 }
 
 // Draw something so we can see it in the background
 function draw() {
   background(255);
   strokeWeight(4);
-  drawFace_closedJaw();
-  //drawFace_openJaw();
+  // Blink eyes when all the balls are eaten
+  if (balls.length == 0){
+	  leftEye.eyeBallColour = 0;
+      leftEye.lashes = 1;
+	  rightEye.eyeBallColour = 0;
+      rightEye.lashes = 1
+	  drawFace_closedJaw();
+  // Drawing the eyes in their position
+  strokeWeight(1);
+  leftEye.drawEyes();
+  rightEye.drawEyes();
+  }
+  else{
+  if (length < 18) {
+	drawFace_openJaw();
+  }
+  else drawFace_closedJaw();
+  //
+
   // Drawing the eyes in their position
   strokeWeight(1);
   leftEye.drawEyes();
   rightEye.drawEyes();
 
   // Vx and Vy are randomly defined as the perlin noise algorithme
-  fly1.calculateVelocity();
-  fly2.calculateVelocity();
-  fly3.calculateVelocity();
-  fly4.calculateVelocity();
-  fly5.calculateVelocity();
+  for (var i = 0; i < balls.length; i++) {
+    	balls[i].calculateVelocity();
+		balls[i].update();
+		balls[i].dist = balls[i].howFarIs();
+		distances[i] = balls[i].dist;
+		balls[i].display();
+		balls[i].tx += random(0.01,0.02);
+		balls[i].ty += random(0.01,0.03);
+  }
 
-  // Updating the flys position
-  fly1.update();
-  fly2.update();
-  fly3.update();
-  fly4.update();
-  fly5.update();
-
-  // Adding Proportional distance between the flys and the eyes
-  length1 = fly1.howFarIs();
-  length2 = fly2.howFarIs();
-  length3 = fly3.howFarIs();
-  length4 = fly4.howFarIs();
-  length5 = fly5.howFarIs();
-
-  length = min(length1,length2,length3,length4,length5);
-  if(length == length1) {
-	fly = fly1;
+  length = min(distances);
+  j = 0;
+  while ((distances[j] != length) && (j < num_balls)) {
+	j= j+1;
   }
-  else if(length == length2) {
-	  fly = fly2;
-  }
-  else if(length == length3) {
-	  fly = fly3;
-  }
-  else if(length == length4) {
-	  fly = fly4;
-  }
-  else fly = fly5;
 
   // Angle calculation
-  a = atan2(fly.y - height/2, fly.x - width/2);
+  a = atan2(balls[j].y - height/2, balls[j].x - width/2);
 
   // Rotating the eyes so it follows the fly
   leftEye.rotate(a, length);
   rightEye.rotate(a, length);
 
-  // Displaying the fly
-  fly1.display();
-  fly1.tx += 0.03;
-  fly1.ty += 0.02;
 
-  fly2.display();
-  fly2.tx += 0.02;
-  fly2.ty += 0.01;
-
-  fly3.display();
-  fly3.tx += 0.03;
-  fly3.ty += 0.01;
-
-  fly4.display();
-  fly4.tx += 0.02;
-  fly4.ty += 0.02;
-
-  fly5.display();
-  fly5.tx += 0.01;
-  fly5.ty += 0.01;
+  //tongue
+  if (length < 15) {
+	//drawFace_openJaw();
+  push();
+  stroke(255, 204, 0);
+	strokeWeight(40);
+    line(width/2, 6.2 *height/8, balls[j].x, balls[j].y);
+  pop();
+	balls.splice(j,1);
+	distances = [];
+	length = 30;
+  }
+  }
 }
 
 function drawFace_closedJaw(){
@@ -155,3 +143,7 @@ function windowResized() {
   // We can use the resizeCanvas() function to resize our canvas to the new window dimensions
   resizeCanvas(windowWidth,windowHeight);
   }
+// function to return a random integer(for the colors)
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
